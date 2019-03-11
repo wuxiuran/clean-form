3361c5f130c7a75cb86db2efeb50d869b8ae2dc3
wuxiuran <xiantaodewxr@163.com>
2019-03-12 02:47:07 +0800

    fix publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
   baseUrl: '/',
-  publicPath: '/static/',
+  publicPath: '/',
   assetsDir: 'static'
 }
